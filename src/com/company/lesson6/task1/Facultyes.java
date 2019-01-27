cb8f763307dafa51d8ee1ed1676aa7998b0d0951
Rudyroom1 <1ki14b.kush@gmail.com>
2019-01-27 18:06:55 +0200

    Added Maven in the project.

--- src/com/company/lesson6/task1/Facultyes.java
+++
@@ -1,9 +0,0 @@
-package com.company.lesson6.task1;
-
-public enum Facultyes {
-    FITKI,
-    FAKSU,
-    FRAU,
-    FEEM
-
-}
--- src/com/company/lesson6/task2/interfaceShip/Ship.java
+++
@@ -1,5 +0,0 @@
-package com.company.lesson6.task2.interfaceShip;
-
-public interface Ship {
-    void sail();
-}
